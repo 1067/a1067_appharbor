2bdc04348fa5fa51a1c1b34c86eba9fbf30a3b1c
a1067 <victor.net.su@gmail.com>
2013-06-24 22:32:19 +0300

    Use Require Ssl solutions from AppHarbor (https://gist.github.com/runesoerensen/915869)

---
+++ a1067/AppHarbor.Tweaks/Mvc/Attributes/AhRequireHttpsAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using RequireHttpsAttributeBase = System.Web.Mvc.RequireHttpsAttribute;
+
+namespace AppHarbor.Web
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true,  AllowMultiple = false)]
+    public class AhRequireHttpsAttribute : RequireHttpsAttributeBase
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+
+            var request = filterContext.HttpContext.Request;
+
+            if (request.IsSecureConnection)
+            {
+                return;
+            }
+
+            if (string.Equals(request.Headers["X-Forwarded-Proto"],
+                "https",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            if (request.IsLocal)
+            {
+                return;
+            }
+
+            HandleNonHttpsRequest(filterContext);
+        }
+    }
+}
