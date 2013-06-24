fcb432829acb67ce85fb61d606e122c20164cae4
a1067 <victor.net.su@gmail.com>
2013-06-24 22:09:09 +0300

    Add project for nUnit tests of 1067

---
+++ a1067/Tests.a1067/Controllers/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests.a1067.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
